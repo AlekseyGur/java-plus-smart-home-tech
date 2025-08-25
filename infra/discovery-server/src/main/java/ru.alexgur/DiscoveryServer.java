604fc95d55244dd842026cecfaef7c6e68f99825
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-08-25 13:20:52 +0300

    Fix eureka main class name

--- infra/discovery-server/src/main/java/ru.alexgur/DiscoveryServer.java
+++ infra/discovery-server/src/main/java/ru.alexgur/DiscoveryServer.java
@@ -6,11 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServer {
-
+public class DiscoveryServer {
     public static void main(String[] args) {
-        
-        SpringApplication.run(EurekaServer.class, args);
+        SpringApplication.run(DiscoveryServer.class, args);
     }
-
 }
