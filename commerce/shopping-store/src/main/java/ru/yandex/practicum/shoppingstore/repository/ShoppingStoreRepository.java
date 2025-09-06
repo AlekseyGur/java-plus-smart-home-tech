b029d152a2501fab1ce42a5b89d819a9ca53dccf
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-09-06 16:24:37 +0300

    Add findByProductId

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/repository/ShoppingStoreRepository.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shoppingstore/repository/ShoppingStoreRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.interactionapi.enums.ProductCategory;
 import ru.yandex.practicum.shoppingstore.model.Product;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ShoppingStoreRepository extends JpaRepository<Product, UUID> {
+
     Page<Product> findAllByProductCategory(ProductCategory productCategory, Pageable pageable);
+
+    Optional<Product> findByProductId(UUID productId);
 }
