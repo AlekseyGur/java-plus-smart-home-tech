25a011bbe9204cf88ebf4d15a0c3d4bb77100916
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-09-06 16:27:12 +0300

    Fix warehouse

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/model/Warehouse.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/model/Warehouse.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.warehouse.model;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -10,24 +10,19 @@ import lombok.experimental.FieldDefaults;
 import java.util.UUID;
 
 @Entity
-@Table(name = "warehouse_product")
 @Getter
 @Setter
 @Builder
-@ToString
+@Table(name = "warehouse_product")
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Warehouse {
     @Id
-    @Column(name = "product_id")
     UUID productId;
-
-    int quantity;
-    Boolean fragile;
-
-    Double width;
-    Double height;
-    Double depth;
+    Long quantity;
+    boolean fragile;
+    @Embedded
+    Dimension dimension;
     double weight;
 }
--- commerce/warehouse/src/main/resources/schema.sql
+++ commerce/warehouse/src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-drop table if exists shopping_cart, shopping_cart_items, warehouse_product;
+drop table if exists warehouse_product, bookings, booking_products;
 
 create table if not exists warehouse_product
 (
     product_id uuid primary key,
     quantity   integer,
     fragile    boolean,
-    width      double precision,
-    height     double precision,
-    depth      double precision,
-    weight     double precision
+    width      double precision not null,
+    height     double precision not null,
+    depth      double precision not null,
+    weight     double precision not null
 );
 
 create table if not exists bookings
@@ -25,4 +25,5 @@ create table if not exists booking_products
     shopping_cart_id uuid references bookings (shopping_cart_id) on delete cascade primary key,
     product_id       uuid not null,
     quantity         integer
-)
+    )
+
